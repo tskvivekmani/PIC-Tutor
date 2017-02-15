ed5e9e6d4a4ec601d335e08ca1f700e6be2ec54a
Vivekmani TSK <tskvivekmani@gmail.com>
2017-02-15 15:52:38 +0530

    Update LCD Snippets.c

--- LCD/LCD Snippets.c
+++ LCD/LCD Snippets.c
@@ -16,23 +16,28 @@ sbit LCD_D4_Direction at TRISB4_bit;
 // End of LCD Pin direction
 void main() {
     lcdInit();
-    start();
     Delay_ms(1000);
-    
+    while(1){
+        start();
+        Delay_ms(1000);
+    }
 }
 //Code to initialize LCD during start up
 void lcdInit(){
-  Lcd_Init();
-  Lcd_Cmd(_LCD_CLEAR);
-  Lcd_Cmd(_LCD_CURSOR_OFF);
+    Lcd_Init();
+    Lcd_Cmd(_LCD_CLEAR);
+    Lcd_Cmd(_LCD_CURSOR_OFF);
 }
 
 void start(){
-  Lcd_Out(1,1,”   Welcome to   ”);
-  char *name = ” TSK India Elc. ”;
-  Lcd_Cmd(_LCD_SECOND_ROW);
-  for(int i=0;i<16;i++){
-    Lcd_Chr_Cp(name[i]);
-    Delay_ms(1000);
-  }
+    Lcd_Out(1,1,”   Welcome to   ”);
+    char *name = ” TSK India Elc. ”;
+    Lcd_Cmd(_LCD_SECOND_ROW);
+    for(int i=0;i<16;i++)
+    {
+        Lcd_Chr_Cp(name[i]);
+        Delay_ms(500);
+    }
+    Lcd_Cmd(_LCD_CLEAR);
+    Lcd_Cmd(_LCD_RETURN_HOME);
 }
