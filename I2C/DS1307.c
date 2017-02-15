a8a2e5819c999772db193bb202b19c09e2a0ab67
Vivekmani TSK <tskvivekmani@gmail.com>
2017-02-15 15:57:04 +0530

    Create DS1307.c

---
+++ I2C/DS1307.c
@@ -0,0 +1,29 @@
+//Adress reference
+//https://electrosome.com/wp-content/uploads/2012/05/Time-Keeper-Registers.png
+
+
+//https://electrosome.com/wp-content/uploads/2012/05/Reading-Data-from-DS1307.jpg
+unsigned short read_ds1307(unsigned short address)
+ {
+   unsigned short temp
+   I2C1_Start();
+   I2C1_Wr(0xD0);
+   I2C1_Wr(address);
+   I2C1_Repeated_Start();
+   I2C1_Wr(0xD1);
+   temp = I2C1_Rd(0);
+   I2C1_Stop();
+   return(temp);
+ }
+ 
+//https://electrosome.com/wp-content/uploads/2012/05/data-write.png
+ void write_ds1307(unsigned short address, unsigned short w_data)
+ {
+   I2C1_Start();
+   I2C1_Wr(0xD0);
+   I2C1_Wr(address);
+   I2C1_Wr(w_data);
+   I2C1_Stop();
+ }
+ 
+ 
