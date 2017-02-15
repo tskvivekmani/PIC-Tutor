58b0bc98ddef13cf512575917229cc5d39793c7e
Vivekmani TSK <tskvivekmani@gmail.com>
2017-02-15 15:58:40 +0530

    Update DS1307.c

--- I2C/DS1307.c
+++ I2C/DS1307.c
@@ -1,29 +1,26 @@
-//Adress reference
+//Address reference
 //https://electrosome.com/wp-content/uploads/2012/05/Time-Keeper-Registers.png
-
 
 //https://electrosome.com/wp-content/uploads/2012/05/Reading-Data-from-DS1307.jpg
 unsigned short read_ds1307(unsigned short address)
- {
-   unsigned short temp
-   I2C1_Start();
-   I2C1_Wr(0xD0);
-   I2C1_Wr(address);
-   I2C1_Repeated_Start();
-   I2C1_Wr(0xD1);
-   temp = I2C1_Rd(0);
-   I2C1_Stop();
-   return(temp);
- }
+{
+  unsigned short temp
+  I2C1_Start();
+  I2C1_Wr(0xD0);
+  I2C1_Wr(address);
+  I2C1_Repeated_Start();
+  I2C1_Wr(0xD1);
+  temp = I2C1_Rd(0);
+  I2C1_Stop();
+  return(temp);
+}
  
 //https://electrosome.com/wp-content/uploads/2012/05/data-write.png
- void write_ds1307(unsigned short address, unsigned short w_data)
- {
-   I2C1_Start();
-   I2C1_Wr(0xD0);
-   I2C1_Wr(address);
-   I2C1_Wr(w_data);
-   I2C1_Stop();
- }
- 
- 
+void write_ds1307(unsigned short address, unsigned short w_data)
+{
+  I2C1_Start();
+  I2C1_Wr(0xD0);
+  I2C1_Wr(address);
+  I2C1_Wr(w_data);
+  I2C1_Stop();
+}
