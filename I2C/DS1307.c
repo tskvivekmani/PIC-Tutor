49e8ae463899da1f14be82159b1913da20addf2b
Vivekmani TSK <tskvivekmani@gmail.com>
2017-02-17 12:39:41 +0530

    BCD Converter functions

--- I2C/DS1307.c
+++ I2C/DS1307.c
@@ -24,3 +24,34 @@ void write_ds1307(unsigned short address, unsigned short w_data)
   I2C1_Wr(w_data);
   I2C1_Stop();
 }
+
+unsigned char BCD2UpperCh(unsigned char bcd){
+  return ( (bcd>>4) +'0');
+}
+
+unsigned char BCD2LowerCh(unsigned char bcd){
+  return ( (bcd & 0x0F) +'0');
+}
+
+int Binary2BCD(int a){
+  int t1,t2;
+  t1 = a%10;
+  t1 = t1 & 0x0F;
+  a = a/10;
+  t2 = a%10;
+  t2 = 0xF0 & t2;
+  t1 = t1 | t2;
+  return t1;
+}
+
+int BCD2Binary(int a){
+  int r,t;
+  t = a & 0x0F;
+  r = t;
+  a = 0xF0 & a;
+  t = a >> 4;
+  t = 0x0F & t;
+  r = t*10 + r;
+  return r;
+}
+
